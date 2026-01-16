e1f228702e736e6bf56c609c472551120e1f92ea
Jamai Salma <salma.jamai@student.vinci.be>
2026-01-16 17:03:20 +0100

    refactor: declare validateEmail instanciation

--- test/validateEmail.test.ts
+++ test/validateEmail.test.ts
@@ -1,16 +1,15 @@
 import { ValidateEmail } from "../src/validateEmail";
 
 describe("ValidateEmail", () => {
-    describe("Checks that email is valid", () => {
+    let validateEmail = new ValidateEmail();
 
+    describe("Checks that email is valid", () => {
         it("should return false when given an empty email", () => {
-            let validateEmail = new ValidateEmail();
             let actual = validateEmail.isValid("");
             expect(actual).toBe(false);
         })
 
         it("should return true when given an email with @", () => {
-            let validateEmail = new ValidateEmail();
             let actual = validateEmail.isValid("@");
             expect(actual).toBe(true);
         })
